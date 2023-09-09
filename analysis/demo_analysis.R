77571018ef6d86c6758082439c21e10071c00383
Koen Hufkens <koen.hufkens@gmail.com>
2023-09-09 10:27:46 +0200

    quick demo analysis

--- analysis/demo_analysis.R
+++ analysis/demo_analysis.R
@@ -1,31 +1,61 @@
 # load libraries and functions
 library(dplyr)
+library(ggplot2)
+library(rnaturalearth)
+library(sf)
 library(skytrackr)
 
-files <- list.files(
-  "~/Dropbox/Research_Projects/code_repository/bitbucket/apus_lunar_synchrony/data-raw/geolocators/Gent_Voorhaven_A_apus/",
-  glob2rx("*.lux"),
-  full.names = TRUE
-)
+# files <- list.files(
+#   "~/Dropbox/Research_Projects/code_repository/bitbucket/apus_lunar_synchrony/data-raw/geolocators/Gent_Voorhaven_A_apus/",
+#   glob2rx("*.lux"),
+#   full.names = TRUE
+# )
+#
+# files <- files[!grepl("drift", files)]
+# data <- lapply(files, function(file){
+#   stk_read_lux(file)
+# })
+#
+# data <- do.call("rbind", data)
+#
+# # data <- data |>
+# #   dplyr::filter(
+# #     logger == "CC874"
+# #   )
+#
+# options("dplyr.show_progress" = FALSE)
+# # batch processing via pipe
+# locations <- data |>
+#   group_by(logger) |>
+#   do({
+#     skytrackr(
+#       .,
+#       bbox = c(-20, -40, 60, 55),
+#       plot = FALSE
+#     )
+#   })
+#
+# saveRDS(locations, "data/gent_locations.rds", compress = "xz")
 
-files <- files[!grepl("drift", files)]
-data <- lapply(files, function(file){
-  stk_read_lux(file)
-})
-
-data <- do.call("rbind", data) |>
-  dplyr::filter(
-    logger == "CC874"
+countries <- ne_countries(returnclass = "sf")
+df <- locations |>
+  filter(
+    grb < 1.1,
+    abs(latitude_ci_95 - latitude_ci_5) < 15
   )
 
-options("dplyr.show_progress" = FALSE)
-# batch processing via pipe
-locations <- data |>
-  group_by(logger) |>
-  do({
-    skytrackr(
-      .,
-      bbox = c(-20, -40, 60, 55),
-      plot = TRUE
+p <- ggplot(df) +
+  geom_sf(data = countries) +
+  geom_path(
+    aes(
+      longitude,
+      latitude
     )
-  })
+  ) +
+  coord_sf(
+    xlim = c(-20, 60),
+    ylim = c(-40, 55)
+  ) +
+  facet_wrap(~logger)
+
+plot(p)
