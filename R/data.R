b03b97227f35f5989928cfa6f5ac620b27561e25
Koen Hufkens <koen.hufkens@gmail.com>
2023-09-09 19:04:05 +0200

    adding data

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Migrate Technology Ltd demo data
+#'
+#' Demo data for a single day of Common swift light logger
+#' data as read from a Migrate Technology Ltd .lux file
+#' using stk_read_lux().
+#'
+#' The format is consistent with what is required by
+#' the skytrackr() routine.
+#'
+#' @format DataFrame
+#' \describe{
+#'   \item{logger}{logger ID}
+#'   \item{date}{date}
+#'   \item{date_time}{date and time}
+#'   \item{hour}{decimal hour}
+#'   \item{lux}{light levels in lux}
+#' }
+"cc874"
