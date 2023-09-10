4396b3d5a8bca45a16867dce78bbf222dec912ac
Koen Hufkens <koen.hufkens@gmail.com>
2023-09-10 16:43:07 +0200

    image

--- analysis/demo_analysis.R
+++ analysis/demo_analysis.R
@@ -1,76 +1,69 @@
 # load libraries and functions
+options("dplyr.show_progress" = FALSE)
 library(dplyr)
 library(ggplot2)
 library(rnaturalearth)
 library(sf)
 library(skytrackr)
-# source("R/skytrackr.R")
-# source("R/log_lux.R")
-# source("R/likelihood.R")
-# source("R/stk_fit.R")
-#source("R/stk_read.R")
 
-files <- list.files(
-  "~/Dropbox/Research_Projects/code_repository/bitbucket/apus_lunar_synchrony/data-raw/geolocators/Gent_Voorhaven_A_apus/",
-  glob2rx("*.lux"),
-  full.names = TRUE
-)
+# files <- list.files(
+#   "~/Dropbox/Research_Projects/code_repository/bitbucket/apus_lunar_synchrony/data-raw/geolocators/Gent_Voorhaven_A_apus/",
+#   glob2rx("*.lux"),
+#   full.names = TRUE
+# )
+#
+# files <- files[grepl("drift", files)]
+#
+# data <- lapply(files, function(file){
+#   stk_read_lux(file)
+# })
+#
+# data <- do.call("rbind", data)
+#
+# data <- data |>
+#   dplyr::filter(
+#     logger == "CC874"
+#   )
 
-files <- files[!grepl("drift", files)]
-data <- lapply(files, function(file){
-  stk_read_lux(file)
-})
+data <- stk_read_lux("data-raw/CC874_18Jun22_123407.lux")
 
-data <- do.call("rbind", data)
-
-data <- data |>
-  dplyr::filter(
-    logger == "CC874"
-  )
-
-options("dplyr.show_progress" = FALSE)
-# batch processing via pipe
+# batch processing via pipe for multiple sites
 locations <- data |>
   group_by(logger) |>
   do({
     skytrackr(
       .,
-      iterations = 8000,
+      iterations = 100,
+      particles = 10,
       start_location = c(51.08, 3.73),
-      tolerance = 13,
-      bbox = c(-20, -40, 60, 60), # reasonable search area global
-      plot = TRUE
+      tolerance = 11,
+      plot = FALSE
     )
   })
 
-saveRDS(locations, "~/Desktop/gent_locations.rds", compress = "xz")
+saveRDS(locations, "data-raw/gent_locations_SMC.rds", compress = "xz")
 
-#----- more complex optimization ----
+#---- DEzs MCMC approach ----
 
-# Adaptive MCMC, prior optimization, delayed rejection (DRAM)
-#
-# locations <- data |>
-#   group_by(logger) |>
-#   do({
-#     skytrackr(
-#       .,
-#       iterations = 8000,
-#       start_location = c(51.08, 3.73),
-#       tolerance = 13,
-#       control = list(
-#         sampler = 'Metropolis',
-#         settings = list(
-#           iterations = 20000,
-#           adapt = T,
-#           DRlevels = 2,
-#           gibbsProbabilities = NULL,
-#           temperingFunction = NULL,
-#           optimize = T,
-#           message = FALSE
-#         )
-#       ),
-#       plot = TRUE
-#     )
-#   })
-#
-# saveRDS(locations, "data-raw/gent_locations_dram.rds", compress = "xz")
+locations <- data |>
+  group_by(logger) |>
+  do({
+    skytrackr(
+      .,
+      iterations = 20000,
+      start_location = c(51.08, 3.73),
+      tolerance = 11,
+      bbox = c(-20, -40, 60, 60),
+      control = list(
+          sampler = 'DEzs',
+          settings = list(
+              burnin = iterations * 0.2,
+              iterations = iterations * 0.8,
+              message = FALSE
+          )
+        ),
+      plot = FALSE
+    )
+  })
+
+saveRDS(locations, "data-raw/gent_locations_DEzs.rds", compress = "xz")
