9866b979a40224f4d2f43e10b7d75300f7f1e1de
Koen Hufkens <koen.hufkens@gmail.com>
2023-10-05 16:58:12 +0200

    new unit tests

--- tests/testthat/test_skytrackr.R
+++ tests/testthat/test_skytrackr.R
@@ -24,7 +24,8 @@ test_that("test functions without task ids", {
 
   expect_type(location, "list")
 
-  location_mask <- skytrackr::cc874 |>
+  location_mask <- skytrackr::stk_read_lux(
+    system.file("extdata/cc874.lux", package="skytrackr")) |>
     skytrackr::skytrackr(
       iterations = 10,
       particles = 3,
@@ -44,9 +45,12 @@ test_that("test clustering + plots", {
   df <- skytrackr::stk_read_lux(
     system.file("extdata/cc874.lux", package="skytrackr")
     )
+
   cl <- stk_cluster(df, k = 2, center = TRUE)
   p <- stk_profile(cl)
+  p_night <- stk_profile(cl, center = "night")
 
   expect_type(cl, "list")
   expect_type(p, "list")
+  expect_type(p_night, "list")
 })
