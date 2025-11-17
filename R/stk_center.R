cd332ae59a6fc150166bdc0b7386cead6ba6068b
Koen Hufkens <koen.hufkens@gmail.com>
2025-11-17 12:19:29 +0100

    rename variables

--- R/stk_center.R
+++ R/stk_center.R
@@ -1,14 +1,15 @@
 #' Centers diurnal curves on midday
 #'
-#' Values are centered on midday on a day by day basis. Note that this does
-#' not shift the time series properly as hours are wrapped around. This
-#' function should not be used stand-alone.
+#' Daily values are centered on midday on a day-by-day basis. Note that this does
+#' not shift the time series properly as hours are wrapped around on a given day.
+#' This function should not be used stand-alone, and is primarily used to screen
+#' data for noise / poor quality.
 #'
 #' @param df a skytrackr compatible data frame
 #' @param floor threshold value to center the data with (default = 1.5)
 #' @param replace replace original decimal hour values (default = FALSE)
 #'
-#' @returns a centered skytrackr compatible data frame
+#' @returns a day-by-day centered skytrackr compatible data frame
 #' @export
 #'
 #' @examples
@@ -35,11 +36,11 @@ stk_center <- function(df, floor = 1.5, replace = FALSE){
         as.numeric(
           circular::mean.circular(
             circular::as.circular(
-            hour_data$angle,
-            control.circular = list(
-              zero = 0,
-              units ="degrees",
-              type = "angles"
+              hour_data$angle,
+              control.circular = list(
+                zero = 0,
+                units ="degrees",
+                type = "angles"
               )
             )
           )
@@ -51,17 +52,17 @@ stk_center <- function(df, floor = 1.5, replace = FALSE){
       df <- .data |>
         dplyr::mutate(
           offset = 24 * (180 - a) / 360,
-          hour_cor = .data$hour + .data$offset,
-          hour_cor = ifelse(.data$hour_cor > 24, .data$hour_cor - 24, .data$hour_cor)
+          hour_centered = .data$hour + .data$offset,
+          hour_centered = ifelse(.data$hour_centered > 24, .data$hour_centered - 24, .data$hour_centered)
         )
 
       if (replace){
         df <- df |>
           dplyr::mutate(
-            hour = .data$hour_cor
+            hour = .data$hour_centered
           ) |>
           dplyr::select(
-            -"hour_cor"
+            -"hour_centered"
           ) |>
           dplyr::arrange(.data$hour)
       }
@@ -71,4 +72,7 @@ stk_center <- function(df, floor = 1.5, replace = FALSE){
 
   return(df_center)
 }
+
+
+
 
